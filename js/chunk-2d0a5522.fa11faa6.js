11c45cfe2c1765ebb33de3e996bd94125fe8b124
minteliuwm <minteliu.l@gmail.com>
2019-06-12 09:24:18 +0800

    [deploy] 418c837 - [release] 0.1.12

--- js/chunk-2d0a5522.fa11faa6.js
+++ js/chunk-2d0a5522.fa11faa6.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a5522"],{"09cf":function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("section",{staticClass:"content ndc-doc"},[n("h2",[t._v("介绍")]),n("p",[t._v("基于Vue的UI组件")]),n("br"),n("h2",[t._v("贡献")]),n("p",[t._v("如果你在使用中遇到问题，或者有好的建议，欢迎提"),n("a",{attrs:{href:"https://github.com/minteliuwm/ndc-ui/issues"}},[t._v("Issue")]),t._v("。")])])}],u=n("17cc"),i={},r=Object(u["a"])(i,c,s,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0a5522.d983eda4.js.map
+//# sourceMappingURL=chunk-2d0a5522.fa11faa6.js.map
